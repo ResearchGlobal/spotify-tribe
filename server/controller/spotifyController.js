6710badb4b90c6b55262a09f9572ed9eb1ce98c7
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-08-17 09:45:09 -0400

    got playing song

--- server/controller/spotifyController.js
+++ server/controller/spotifyController.js
@@ -15,6 +15,7 @@ spotifyController.apiRequest = (req, res, next) => {
   })
   .then(data => {
     console.log("API_REQUEST DATA:", data.data)
+    res.cookie.data = data.data;
     res.locals.data = data.data;
     return next()
   })
@@ -22,5 +23,41 @@ spotifyController.apiRequest = (req, res, next) => {
     console.log(err);
   })
 }
+
+spotifyController.getPlayingSong = (req, res, next) => {
+  console.log('***** IN PLAYING SONG *****');
+  console.log(res.locals);
+
+  const playingSongURI = 'https://api.spotify.com/v1/me/player/currently-playing'
+
+
+  axios.get(playingSongURI, {
+    headers: {
+      'Accept' : 'application/json',
+      'Content-Type' : 'application/json',
+      'Authorization' : `Bearer ${res.locals.authToken}`
+    }
+  })
+  .then(data => {
+    console.log('***** SONG PLAYING DATA RETRIEVED: *****');
+    // console.log(data);
+    res.locals.songData = {
+      title: data.data.item.name,
+      artist: data.data.item.artists[0].name
+    };
+    console.log(res.locals.songData);
+    return next();
+  })
+  .catch(err => {
+    console.log('***** ERR: Error in getPlayingSong');
+    console.log(err)
+    return next();
+  })
+
+
+  // return next();
+  //  await
+}
+
 
 module.exports = spotifyController;
--- server/routes/spotifyRouter.js
+++ server/routes/spotifyRouter.js
@@ -4,10 +4,22 @@ const router = express.Router();
 const spotifyController = require('../controller/spotifyController');
 const authController = require('../controller/authController.js');
 
-router.get('/', authController.getSpotifyTokenFromDB, spotifyController.apiRequest, (req, res) => {
+router.get('/',
+  authController.getSpotifyTokenFromDB,
+  spotifyController.apiRequest,
+  (req, res) => {
   // console.log(res.locals.data);
   res.status(200).json(res.locals.data);
 });
+
+router.get('/playing',
+  authController.getSpotifyTokenFromDB,
+  spotifyController.getPlayingSong,
+  (req, res) => {
+    // res.locals()
+    res.status(200).send(res.locals.songData);
+  }
+)
 
 
 
